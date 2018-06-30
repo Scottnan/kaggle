69d267d615a9c09ecd04b1f5ce0d86a4b5319c3c
Nan Yu <nanyu201411@gmail.com>
2018-06-30 20:49:48 +0800

    Add clean data method

---
+++ data/DataHandler.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+def category2int(col):
+    if col.dtype == object:
+        mapping = {label: idx for idx, label in enumerate(set(col))}
+        return col.map(mapping)
+    else:
+        return col
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("application_train.csv")
+    df = df.apply(category2int, axis=0)
+    print(df)
